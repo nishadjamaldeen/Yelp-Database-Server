08428d64d8f548073c53a465a7f615406e3b9b10
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2016-11-29 11:24:16 +0000

    split functional interfaces into two

--- src/ca/ece/ubc/cpen221/mp5/statlearning/Algorithms.java
+++ src/ca/ece/ubc/cpen221/mp5/statlearning/Algorithms.java
@@ -23,13 +23,36 @@ public class Algorithms {
 		return null;
 	}
 
-	// here we are abusing notation a bit when passing a function to getPredictor. This is a way to pass a function that obtains, say, latitude or longitude of a restaurant. You can improve on this is you like. It is the implementation of getBestPredictor that matters.
-	public static LeastSquaresRegression getPredictor(User u, RestaurantDB db, FeatureFunction featureFunction) {
+	/**
+	 * Return a user rating prediction function
+	 * 
+	 * @param user_id
+	 *            the id of the user we are interested in
+	 * @param db
+	 *            the database object that represents the yelp dataset
+	 * @param featureFunction
+	 *            that returns the feature we want to use in making predictions
+	 * @return
+	 */
+	public static LeastSquaresRegression getPredictor(String user_id, RestaurantDB db,
+			FeatureFunction featureFunction) {
 		// TODO: Implement this method
 		return null;
 	}
 
-	public static LeastSquaresRegression getBestPredictor(User u, RestaurantDB db, List<FeatureFunction> featureFunctionList) {
+	/**
+	 * Returns the best predictor for a user's rating
+	 * 
+	 * @param user_id
+	 *            the user id for the user we are interested in
+	 * @param db
+	 *            the database object that represents the yelp dataset
+	 * @param featureFunctionList
+	 *            is a list of feature functions from which the best is selected
+	 * @return the best prediction function
+	 */
+	public static LeastSquaresRegression getBestPredictor(User u, RestaurantDB db,
+			List<FeatureFunction> featureFunctionList) {
 		// TODO: Implement this method
 		return null;
 	}
