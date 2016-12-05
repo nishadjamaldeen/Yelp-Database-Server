51beb8b096cf41fda3c3efb4ba556879552c018f
Sean McConnell <Sean McConnell>
2016-12-04 22:07:58 -0800

    mp5

--- src/ca/ece/ubc/cpen221/mp5/RestaurantDBServer.java
+++ src/ca/ece/ubc/cpen221/mp5/RestaurantDBServer.java
@@ -2,7 +2,11 @@ package ca.ece.ubc.cpen221.mp5;
 
 public class RestaurantDBServer {
 	
-	public static void Main(String args[]){
+	public static void main(String args[]){
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		System.out.println(args[3]);
 		RestaurantDBSocketServer rdbss = new RestaurantDBSocketServer(Integer.parseInt(args[0]), args[1], args[2], args[3]); 
 		rdbss.runDBServer();
 	}
